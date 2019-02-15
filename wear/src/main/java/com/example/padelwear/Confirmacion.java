d4b26673c9590de5067df619b8ca18e2148098f3
William Sulca <wsulcata@everis.com>
2019-02-15 13:17:37 -0500

    advance

---
+++ wear/src/main/java/com/example/padelwear/Confirmacion.java
@@ -0,0 +1,17 @@
+package com.example.padelwear;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by everis on 15/02/19.
+ */
+public class Confirmacion extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.confirmacion);
+    }
+
+}
